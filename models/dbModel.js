8a7f0a948209e54f7f3b099bd10cf020adf6a18b
Mart510 <martynandrewbillings@gmail.com>
2023-11-05 15:34:58 +0000

    starting work on dbModel

---
+++ models/dbModel.js
@@ -0,0 +1,16 @@
+// Model to manage data storage and retrevial from PostGreSQL database
+
+
+// GET all symbols
+
+// GET all records
+
+// PATCH records quote data
+
+// GET record with highest gain by value
+
+// GET record with biggest loss by value
+
+// GET record with highest gain by percentage
+
+// GET record with biggest loss by percentage
