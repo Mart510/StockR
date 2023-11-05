0f0f4e2beee6bcf10690ce5d2770838ddb9c73ca
Mart510 <martynandrewbillings@gmail.com>
2023-11-05 16:20:19 +0000

    starting on controller to test api calls

---
+++ controllers/tickerholderDBController.js
@@ -0,0 +1,7 @@
+// import the model functions
+
+
+// GET all the data in tickerholder
+
+
+// GET all the symbols
--- models/dbModel.js
+++ models/dbModel.js
@@ -1,16 +1,43 @@
 // Model to manage data storage and retrevial from PostGreSQL database
 
+// Importing the pool object so the functions can interact with the database
+import { pool } from "../database/index.js";
 
-// GET all symbols 
+// basic function to take SQL variable and send it to the database via pool
+async function databaseQuery(queryText) {
+    const result = await pool.query(queryText);
+    return result;
+}
 
-// GET all records
+// GET all records 
+export async function getAll() {
+    // Query the database and return all Symbols
+    const result = await databaseQuery("SELECT * FROM tickerholder;");
+
+    // Grabs the rows from the response and returns it
+    return result.rows;
+}
+
+// GET all symbols
+export async function getSymbols() {
+    // Query the database and return all Symbols
+    const result = await databaseQuery("SELECT symbol FROM tickerholder;");
+
+    // Grabs the rows from the response and returns it
+    return result.rows;
+}
 
 // PATCH records quote data
 
+
 // GET record with highest gain by value
+
 
 // GET record with biggest loss by value
 
+
 // GET record with highest gain by percentage
 
+
 // GET record with biggest loss by percentage
+
